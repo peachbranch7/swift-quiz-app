ed036ef9d51dc8cf78958b102dd0f8d9edd383f8
peachbranch777 <peachbranch.0517@gmail.com>
2021-03-29 15:29:34 +0900

    Initial Commit

---
+++ VariousQuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VariousQuizApp
+//
+//  Created by 百枝　映 on 2021/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
